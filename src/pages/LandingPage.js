1dd1261c1a89671cd6203047fa1ca93ceca60af2
Oh Dylan <ddragon1128@hotmail.com>
2022-02-02 23:17:13 +0800

    Updated background

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -16,8 +16,8 @@ const LandingPage = () => {
                     <Space direction="vertical" style={{width: "80%"}} >
                         <Title>Star Launch</Title>
                         <Text style={{fontSize:"1.2rem"}} strong>Star Launch app is built with React and Ant Design components library. It is based on <strong>SpaceX API</strong>(<a target="_blank" rel="noreferrer" href="https://github.com/r-spacex/SpaceX-API" > SpaceX REST API </a>). You may find the source code here: <a target="_blank" rel="noreferrer" href="https://github.com/OhDylan/star-launch" >Github - Star Launch</a>. Please contact me if any issue is found, or raise a ticket in the Github repository.</Text>
-                        <Button size='large' href='/launch'style={{width:150, height: "auto", paddingTop: 10, fontSize: "1.2rem", fontWeight: "bolder", margin: "10px 0", backgroundColor: "rgb(255, 50, 30)", color: "white", borderRadius: "10px"}} >Launch Pad</Button>
-                        <Button size='large' href='/starlink'style={{width:150, height: "auto", verticalAlign: "middle", fontSize: "1.2rem", fontWeight: "bolder", margin: "10px 0", backgroundColor: "rgb(50, 26, 255)", color: "white", borderRadius: "10px"}} >Star Link</Button>
+                        <Button size='large' href='/launch'style={{width:150, height: "auto", paddingTop: 10, fontSize: "1.2rem", fontWeight: "bolder", margin: "10px 0", backgroundColor: "rgb(255, 50, 30)", color: "white", borderRadius: "10px", border: "none"}} >Launch Pad</Button>
+                        <Button size='large' href='/starlink'style={{width:150, height: "auto", verticalAlign: "middle", fontSize: "1.2rem", fontWeight: "bolder", margin: "10px 0", backgroundColor: "rgb(50, 26, 255)", color: "white", borderRadius: "10px", border: "none"}} >Star Link</Button>
                     </Space>
                 </Col>
             </Row>
